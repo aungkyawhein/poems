9f9e7ed6086e78aaf325a9878d94c6c8dfd47412
aungkyawhein <akh.infotech@gmail.com>
2023-09-02 14:06:35 +0800

    Remove cartoons

--- src/app/components/posts.tsx
+++ src/app/components/posts.tsx
@@ -4,11 +4,12 @@ import Post from "./post";
 export default async function Posts() {
 
   const data = await fetchPosts('');
+  const poems = data.posts.filter((post: any) => !post.tags.cartoons);
 
   return (
     <div className="grid gap-y-7 px-7 pb-7 pt-[100px] relative w-full max-w-max mx-auto place-items-center">
       {
-        data.posts.map((poem: any) => (
+        poems.map((poem: any) => (
           <Post key={poem.ID} title={poem.title} content={poem.content} date={poem.date} />
         ))
       }
